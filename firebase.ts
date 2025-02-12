cd885a7e1547788047c482604e1d9eeebe350713
Muhamad Zafar Syah <110374556+MuhamadZafarSyah@users.noreply.github.com>
2025-02-12 07:16:33 +0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -13,12 +13,20 @@ const firebaseConfig = {
   // messagingSenderId: "81135278767",
   // appId: "1:81135278767:web:a00525ddb7baf34c00525a",
 
-  apiKey: "AIzaSyAYwoU8AAzadCzLsnSxdOohtlLP0S2RztI",
-  authDomain: "notion-clone-b5084.firebaseapp.com",
-  projectId: "notion-clone-b5084",
-  storageBucket: "notion-clone-b5084.firebasestorage.app",
-  messagingSenderId: "81135278767",
-  appId: "1:81135278767:web:2ae8454b5e6c165d00525a",
+  // apiKey: "AIzaSyAYwoU8AAzadCzLsnSxdOohtlLP0S2RztI",
+  // authDomain: "notion-clone-b5084.firebaseapp.com",
+  // projectId: "notion-clone-b5084",
+  // storageBucket: "notion-clone-b5084.firebasestorage.app",
+  // messagingSenderId: "81135278767",
+  // appId: "1:81135278767:web:2ae8454b5e6c165d00525a",
+
+  apiKey: "AIzaSyCQ3crpXrxzcsJqmvT2V7UQzSZ3ULfG9Z8",
+  authDomain: "zafarnotion.firebaseapp.com",
+  projectId: "zafarnotion",
+  storageBucket: "zafarnotion.firebasestorage.app",
+  messagingSenderId: "825854594079",
+  appId: "1:825854594079:web:c36952d9d82e2f7bf7d92a",
+  measurementId: "G-FRVXKD1SLH",
 };
 
 // Initialize Firebase
